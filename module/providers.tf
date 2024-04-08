1abe5554b6a9fa884f78236125a188a07058854e
VikasVashisth <vikas.vashisth@nashtechglobal.com>
2024-04-08 18:39:56 +0530

    Update providers

--- module/providers.tf
+++ module/providers.tf
@@ -1,10 +1,5 @@
 terraform {
   required_providers {
-
-    # azurerm = {
-    #   source  = "hashicorp/azurerm"
-    #   version = "3.52.0"
-    # }
     azuredevops = {
       source  = "microsoft/azuredevops"
       version = "~>1.0"
